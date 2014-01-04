ca95a9927ce5dd8b8b177429eeafd17d449c58ff
Rusty <slayjay78@gmail.com>
2014-01-04 13:40:46 -0800

    Correctly call quiet hours from setting

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1119,5 +1119,6 @@ public class Settings extends PreferenceActivity
     public static class PaymentSettingsActivity extends Settings { /* empty */ }
     public static class PrintSettingsActivity extends Settings { /* empty */ }
     public static class PrintJobSettingsActivity extends Settings { /* empty */ }
-    public static class ActiveDisplaySettingsActivity extends Settings { /* empty */ }     
+    public static class ActiveDisplaySettingsActivity extends Settings { /* empty */ }  
+    public static class QuietHoursSettingsActivity extends Settings { /* empty */ }   
 }
